a650cab6a506cfc8512b3f1a5f992bf32f8dbb32
Md Tazri <MdTazri@MdTazri>
2020-12-12 00:03:14 +0600

    Now i going to learning besic programming concept with python with stack learner

---
+++ 3/print.py
@@ -0,0 +1,15 @@
+print('Hello World');
+print('My Name is Tazri');
+print('I want to learn python');
+print('It for fun');
+print('Python is easy');
+print('One day I will become python developer');
+#This is commit 
+#This is simple line commit
+#Commit for understand source code.
+#Commit is necessary
+#Commit for understand my source code for anoter person or me in future
+#Commit descrive source code.
+
+#simple python divide syntax
+print(3/2);
